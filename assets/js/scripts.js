c189b6514850e38b35acd81c05a3346c085d3e0b
cristian <cristian.fpg@hotmail.com>
2015-09-20 18:19:58 -0500

    los botones funcionan :D

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+$(".edu").on("click" ,function(){
+  $('html, body').animate({scrollTop:$('#cont1').position().top}, 'slow');
+});
+$(".form").on("click" ,function(){
+  $('html, body').animate({scrollTop:$('#cont2').position().top}, 'slow');
+});
+$(".contact").on("click" ,function(){
+  $('html, body').animate({scrollTop:$('#cont3').position().top}, 'slow');
+});
