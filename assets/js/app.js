2a8a940b0853868ccd7cee00ab87b8ea8e076538
cristianfpg <cristian.fpg@hotmail.com>
2015-09-19 19:57:12 -0500

    Delete app.js

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-var sendMixpanel = function() {
-    var email = document.getElementById("email").value;
-    mixpanel.track("nuevo email", {
-      email: email,
-      timestamp: Date.now()
-    });
-  };
