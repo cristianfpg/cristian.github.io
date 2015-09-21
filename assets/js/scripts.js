b9d661432926b77bf9a9ef6e3a1448bf3ee3fe88
cristian <cristian.fpg@hotmail.com>
2015-09-20 20:15:05 -0500

    falta formulario contacto

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,22 @@
 $(".edu").on("click" ,function(){
-  $('html, body').animate({scrollTop:$('#cont1').position().top}, 'slow');
+  $('body').animate({scrollTop:$('#cont1').position().top}, 'slow');
 });
 $(".form").on("click" ,function(){
-  $('html, body').animate({scrollTop:$('#cont2').position().top}, 'slow');
+  $('body').animate({scrollTop:$('#cont2').position().top}, 'slow');
 });
 $(".contact").on("click" ,function(){
-  $('html, body').animate({scrollTop:$('#cont3').position().top}, 'slow');
+  $('body').animate({scrollTop:$('#cont3').position().top}, 'slow');
 });
+$(".home").on("click" ,function(){
+  $('body').animate({scrollTop:$('.lp').position().top}, 'slow');
+});
+
+////////////////////////////////////////////////////////////////
+
+var waypoint = new Waypoint({
+  element: document.getElementById('cont2'),
+  handler: function() {
+    // ingrese codigo aqui
+    //$(".dot").animate({height: "50px",width: "50px"});
+  }
+})
