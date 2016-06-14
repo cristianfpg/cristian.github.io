e71c5bffd7a4a2ad034d3bf00d0761c02210dab8
cristianfpg <cristian.fpg@hotmail.com>
2016-06-14 14:17:32 -0500

    done categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,20 @@ function bgAnimate(child,filename){
 bgAnimate(1,"about");
 bgAnimate(3,"portfolio");
 bgAnimate(5,"contact");
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+  	console.log(location.pathname)
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      console.log(target);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
